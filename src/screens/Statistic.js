7484b2fc30c1ee2f07d3307f5d3ad987f1328628
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-26 20:47:55 +0500

    Updates

--- src/screens/Statistic.js
+++ src/screens/Statistic.js
@@ -18,6 +18,7 @@ const Statistic = () => {
           <Text>Statistic</Text>
           <Text>Statistic</Text>
           <Text>Statistic</Text>
+          <Text>Statistic</Text>
         </View>
       </View>
     </>
