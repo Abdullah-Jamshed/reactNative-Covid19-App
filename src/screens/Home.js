da1658a90ed2a962307a72213b680a5d658950d3
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-30 23:14:39 +0500

    Update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -95,7 +95,6 @@ const Home = () => {
                   <Text style={{color:"#bfbfbf"}}>
                     Follow the instructions to do your own test.
                     Follow the instructions to do your own test.
-                    Follow the instructions to do your own test.
                   </Text>
                 </View>
               </View>
