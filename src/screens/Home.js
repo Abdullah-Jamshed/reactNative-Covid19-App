2eac37ff80da8b8335d30f8a6782a15105e95add
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-31 20:04:44 +0500

    Update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -96,7 +96,6 @@ const Home = () => {
                     Follow the instructions to do your own test.
                     Follow the instructions to do your own test.
                     Follow the instructions to do your own test.
-                    Follow the instructions to do your own test.
                   </Text>
                 </View>
               </View>
