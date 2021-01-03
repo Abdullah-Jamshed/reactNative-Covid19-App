6bd88ebbc33c90e41674d46f6c89ba992efcb43b
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-01-03 22:44:37 +0500

    updates

--- src/screens/Statistic.js
+++ src/screens/Statistic.js
@@ -120,7 +120,6 @@ const Statistic = () => {
                   <Menu.Item onPress={() => {}} title="Item 1" />
                   <Menu.Item onPress={() => {}} title="Item 2" />
                   <Menu.Item onPress={() => {}} title="Item 2" />
-                  <Menu.Item onPress={() => {}} title="Item 2" />
                   <Divider />
                   <Menu.Item onPress={() => {}} title="Item 3" />
                 </Menu>
