42793319793b7c80ebb315de5d046ee639f2b90f
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-01-07 23:42:14 +0500

    updates

--- src/screens/Statistic.js
+++ src/screens/Statistic.js
@@ -361,7 +361,6 @@ const Statistic = ({menuFlag, closeMenu, locationSetValue, location}) => {
         <View style={styles.lower}>
           <Text>Statistic</Text>
           <Text>Statistic</Text>
-          <Text>Statistic</Text>
           <Image
             source={{uri: 'https://www.countryflags.io/pk/shiny/64.png'}}
             style={{width: 60, height: 60}}
