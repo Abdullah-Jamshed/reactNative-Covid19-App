d18a3869729fbf3d24b4b535155fc5ce0d6bb921
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-01-10 23:02:04 +0500

    Application Done

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, View, Text} from 'react-native';
 import Header from './src/components/Header';
 
 import Navigator from './src/configs/Navigator';
 
 import {Provider} from 'react-native-paper';
 
+// redux
 import {Provider as ReduxProvider} from 'react-redux';
 import store from './src/store/';
 
@@ -21,13 +21,5 @@ const App = () => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: 'red',
-  },
-});
 
 export default App;
