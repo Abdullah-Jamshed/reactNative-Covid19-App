acf84cabd1ba5a5f8ebb6ff50f529f4b498921e0
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-01-10 22:49:19 +0500

    updates

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -228,31 +228,4 @@ const myDataset = [
   ],
 ];
 
-const Graph2 = () => {
-  const transformData = (dataset) => {
-    const totals = dataset[0].map((data, i) => {
-      return dataset.reduce((memo, curr) => {
-        return memo + curr[i].y;
-      }, 0);
-    });
-    return dataset.map((data) => {
-      return data.map((datum, i) => {
-        return {x: datum.x, y: (datum.y / totals[i]) * 100};
-      });
-    });
-  };
-
-  const dataset = transformData(myDataset);
-  return (
-    <VictoryChart height={400} width={400} domainPadding={{x: 30, y: 20}}>
-      <VictoryStack colorScale={['black', 'blue', 'tomato']}>
-        {dataset.map((data, i) => {
-          return <VictoryBar data={data} key={i} />;
-        })}
-      </VictoryStack>
-      <VictoryAxis dependentAxis tickFormat={(tick) => `${tick}%`} />
-      <VictoryAxis tickFormat={['a', 'b', 'c', 'd', 'e']} />
-    </VictoryChart>
-  );
-};
 
